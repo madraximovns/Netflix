4d7a2388ea062f93553985fd2fdc0bb21951c2dc
Bekzod Xudayberdiyev <bekzod@767803-cc73114.tmweb.ru>
2022-01-11 11:07:36 +0300

    static settings

--- Netflix/urls.py
+++ Netflix/urls.py
@@ -3,6 +3,8 @@ from django.urls import path, include
 from drf_yasg import openapi
 from drf_yasg.views import get_schema_view
 from rest_framework.permissions import AllowAny
+from django.conf import settings
+from django.conf.urls.static import static
 
 schema_view = get_schema_view(
     openapi.Info(
@@ -20,3 +22,5 @@ urlpatterns = [
     path('docs/', schema_view.with_ui('swagger', cache_timeout=0), name='swagger-docs'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='redoc-docs'),
 ]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
